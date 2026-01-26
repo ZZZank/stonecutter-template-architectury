7e4a0649c5efb46b85e7c35dd5abb1b93941250b
ZZZank <3410764033@qq.com>
2026-01-26 21:40:11 +0800

    add common maven repos for mod dependencies, more mixin example

--- src/main/java/com/example/template/mixin/ExampleMixin.java
+++ src/main/java/com/example/template/mixin/ExampleMixin.java
@@ -1,15 +1,21 @@
 package com.example.template.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
+public abstract class ExampleMixin {
+	@Shadow
+	public abstract void sendMessage(Text message);
+
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+		this.sendMessage(Text.literal("Hi from ExampleMod"));
 	}
 }
