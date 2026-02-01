840e399a8fa02420b07b4a2ef4eb8c2d6b2ad414
ZZZank <3410764033@qq.com>
2026-02-01 19:47:32 +0800

    according to SC wiki, `build.gradle.kts` is once per node

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ base {
 }
 
 architectury.common(stonecutter.tree.branches.mapNotNull {
-    if (stonecutter.current.project !in it) {
-        null
-    } else {
+    if (stonecutter.current.project in it) {
         it.project.prop("loom.platform")
+    } else {
+        null
     }
 })
 
@@ -34,27 +34,42 @@ loom {
     }
 }
 
-allprojects {
-    java {
-        withSourcesJar()
-        val java = if (stonecutter.eval(minecraft, ">=1.20.5")) {
-            JavaVersion.VERSION_21
-        } else if (stonecutter.eval(minecraft, ">=1.18")) {
-            JavaVersion.VERSION_17
-        } else if (stonecutter.eval(minecraft, ">=1.17")) {
-            JavaVersion.VERSION_16
-        } else {
-            JavaVersion.VERSION_1_8
+repositories {
+    fun strictMaven(name: String, url: String, vararg groups: String) = exclusiveContent {
+        forRepository {
+            maven(url) {
+                this.name = name
+            }
         }
-        targetCompatibility = java
-        sourceCompatibility = java
+        filter {
+            groups.forEach(this::includeGroup)
+        }
     }
 
-    tasks.compileJava {
-        options.encoding = "UTF-8"
+    mavenCentral()
+    strictMaven("CurseMaven", "https://cursemaven.com", "curse.maven")
+    strictMaven("Modrinth", "https://api.modrinth.com/maven", "maven.modrinth")
+}
 
-        // very few developers will provide source jar when publishing mods, we add param names in production jar
-        // to make life easier for those who need to work with the mod
-        options.compilerArgs.add("-parameters")
+java {
+    withSourcesJar()
+    val java = if (stonecutter.eval(minecraft, ">=1.20.5")) {
+        JavaVersion.VERSION_21
+    } else if (stonecutter.eval(minecraft, ">=1.18")) {
+        JavaVersion.VERSION_17
+    } else if (stonecutter.eval(minecraft, ">=1.17")) {
+        JavaVersion.VERSION_16
+    } else {
+        JavaVersion.VERSION_1_8
     }
+    targetCompatibility = java
+    sourceCompatibility = java
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+
+    // very few developers will provide source jar when publishing mods, we add param names in production jar
+    // to make life easier for those who need to work with the mod
+    options.compilerArgs.add("-parameters")
 }
--- buildSrc/src/main/kotlin/build-extensions.kt
+++ buildSrc/src/main/kotlin/build-extensions.kt
@@ -1,16 +1,9 @@
 import org.gradle.api.Project
-import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.kotlin.dsl.maven
 import org.gradle.language.jvm.tasks.ProcessResources
 
 val Project.mod: ModData get() = ModData(this)
 fun Project.prop(key: String): String? = findProperty(key)?.toString()
 fun String.upperCaseFirst() = replaceFirstChar { if (it.isLowerCase()) it.uppercaseChar() else it }
-
-fun RepositoryHandler.strictMaven(url: String, alias: String, vararg groups: String) = exclusiveContent {
-    forRepository { maven(url) { name = alias } }
-    filter { groups.forEach(::includeGroup) }
-}
 
 fun ProcessResources.properties(files: Iterable<String>, vararg properties: Pair<String, Any>) {
     for ((name, value) in properties) inputs.property(name, value)
